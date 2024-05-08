330d229215774733ea3fd9f85b9a245d727ca1c4
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-08 00:21:54 -0700

    Assignment 15

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-var guestslist = ["sukaina", "ashfa", "noveen", "kanwal"];
-guestslist.forEach(function (guest) { return console.log("salam ".concat(guest, ",would you like to have dinner with me?")); });
+var guestlist = ["sukaina", "ashfa", "noveen", "kanwal"];
+var guestnotcomming = guestlist[0];
+console.log(guestnotcomming, "not comming");
+guestlist.splice(0, 0, "komal");
+guestlist.forEach(function (guest) { return console.log("salam ".concat(guest, ",would you like to come on dinner?")); });
--- app.ts
+++ app.ts
@@ -1,2 +1,5 @@
-let guestslist = ["sukaina","ashfa","noveen","kanwal"];
-guestslist.forEach(guest =>console.log(`salam ${guest},would you like to have dinner with me?`));
+let guestlist = ["sukaina","ashfa","noveen","kanwal"];
+let guestnotcomming = guestlist[0];
+console.log(guestnotcomming ,  "not comming");
+guestlist.splice(0 ,0, "komal");
+guestlist.forEach(guest => console.log(`salam ${guest},would you like to come on dinner?`));
