0af6d15b2d18a348d067b45e245f261bbea180d5
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-07 23:05:58 -0700

    Assignment 4

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 "use strict";
-let personName = "asfah mussarrat";
-console.log(personName.toLowerCase());
-console.log(personName.toUpperCase());
-console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()));
+console.log('Albert Einstine once said," A person who never make a mistake never tried anything new."');
