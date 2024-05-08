0508dc4475a41d39e895c0b62251b31acf6700f9
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-08 05:52:22 -0700

    Assignment 20

---
+++ A20.js
@@ -0,0 +1,6 @@
+//lists of programming language
+var programminglanguages = ["typescript", "javascript", "python", "php"];
+//print the list one by one
+console.log("lists of programming language");
+//print list by foreach method
+programminglanguages.forEach(function (language) { return console.log(language); });
---
+++ A20.ts
@@ -0,0 +1,7 @@
+//lists of programming language
+let programminglanguages:string[] = ["typescript","javascript","python","php"];
+//print the list one by one
+console.log("lists of programming language");
+//print list by foreach method
+programminglanguages.forEach(language =>console.log(language));
+
