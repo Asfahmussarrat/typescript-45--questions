50c0c72e70ee2974c3c9b93a64d172de7ea1b57f
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-08 00:05:04 -0700

    Assignment 9

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-console.log(4 + 4);
-console.log(12 - 4);
-console.log(4 * 2);
-console.log(16 / 2);
-// Addition of numbers
-var Addition = 4 + 4;
-console.log("Addition ", Addition);
-//subtraction of number
-var subtraction = 12 - 4;
-console.log("subtraction", subtraction);
-// Multiplication of number
-var Multiplication = 4 * 2;
-console.log("Multiplication", Multiplication);
-//Division of number
-var Division = 16 / 2;
-console.log("Division", Division);
+var favNumber = 19;
+var message = "mine favNumber is: ".concat(favNumber);
+console.log(message);
--- app.ts
+++ app.ts
@@ -1,16 +1,3 @@
-console.log(4+4);
-console.log(12-4);
-console.log(4*2);
-console.log(16/2);
-// Addition of numbers
-let Addition:number = 4 + 4;
-console.log( "Addition ", Addition);
-//subtraction of number
-let subtraction:number = 12-4;
-console.log( "subtraction",subtraction);
-// Multiplication of number
-let Multiplication:number = 4*2;
-console.log( "Multiplication",Multiplication);
-//Division of number
-let Division:number = 16/2;
-console.log( "Division", Division);
+let favNumber = 19;
+let message = `mine favNumber is: ${favNumber}`;
+console.log(message);
