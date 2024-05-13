2a1f450a957f024bd58806892ff7069ce4739d1a
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:57:30 -0700

    Assignment40

---
+++ Ap40.ts
@@ -0,0 +1,20 @@
+//Making a music Album Function
+function make_Album(Artist:string,title:string ,tracks?:number) {
+    const Album: {Artist:string ,title:string ,tracks?:number} = {
+        Artist: Artist,
+        title:title
+    } 
+    if (tracks != undefined){
+        Album.tracks = tracks;
+
+    }
+    return Album;
+    
+    
+}
+const Album1 = make_Album("Artist 1" , "Album Title 1");
+console.log(Album1);
+const Album2 = make_Album("Artist2" ,"Album Title 2");
+console.log(Album2);
+const Album3 = make_Album("Artist3" ,"Album Title 3", 12);
+console.log(Album3);
