fc610acffe3f992bc3297e65d84a7612a4b47643
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:50:50 -0700

    Assignment35

---
+++ Ap35.js
@@ -0,0 +1,7 @@
+"use strict";
+// creating of array of animals
+let Animals = ["dog", "lion", "cat", "deer", "bull"];
+for (let Animal of Animals) {
+    console.log(`A ${Animal} would make a great pet.`);
+}
+console.log(`any of these animals would make a great pet!`);
