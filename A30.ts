4fd8d645ad195f09f787da4269b80e573f542249
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:41:07 -0700

    Assignment30

---
+++ A30.ts
@@ -0,0 +1,11 @@
+// creating an array
+let userName = ["sukaina","ashfa","admin","noveen"];
+// using forEach loop on array
+userName.forEach(oneuser => {
+    if(oneuser === "admin"){
+        console.log(`Hello  ${oneuser} ,would you like to see a status report ?`);
+    }else{
+        console.log(`Hello ${oneuser} ,thankyou for logging in again.`);
+    }
+})
+
