a4b14c28a574ed6c9140cf12259634032e1a554b
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:56:29 -0700

    Assignment39

---
+++ Ap39.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    return `${city} ${country}`;
+}
+console.log(city_country("karachi", "Pakistan"));
+console.log(city_country("istambol", "turkey"));
+console.log(city_country("paris", "France"));
---
+++ Ap39.ts
@@ -0,0 +1,5 @@
+function city_country(city:string ,country:string){
+    return `${city} ${country}`;
+}console.log(city_country("karachi","Pakistan"));
+console.log(city_country("istambol","turkey"));
+console.log(city_country("paris","France"));
