a951fdacff04bd857e307fc273154a49e78fcb14
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:37:12 -0700

    Assignment27

---
+++ A27.ts
@@ -0,0 +1,35 @@
+let alienColour = "green";
+if(alienColour === "green"){
+    console.log("(version 1)you shot down green alien you have earned 5 points.");
+}
+else if (alienColour ==="yellow"){
+    console.log("you shot down yellow allien you have earned 10 points.");
+}
+else if(alienColour ==="red"){
+    console.log("you shot down red alien you have earned 15 points.");
+}
+// second version
+let alienColour2 = "yellow";
+if (alienColour2 === "green"){
+    console.log("you shot down green alien you have earned 5 points.");
+
+}
+else if(alienColour2 === "yellow"){
+    console.log("(version2)you shot down yellow allien you have earned 10 points.");
+}
+else if(alienColour2 === "red"){
+    console.log("you shot down red alien you have earned 15 points");
+}
+// version 3
+let alienColour3 = "red";
+if(alienColour3 === "green"){
+    console.log("you shot down green alien you have earned 5 points.");
+}
+else if(alienColour3 === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points.");
+}
+else if(alienColour3 === "red"){
+    console.log("(version 3) you shot down red alien you have earned 15 points.");
+}
+
+
