8eb042736e065bc15e64aafd7df1e3ffe93ca4a1
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:48:57 -0700

    Assignment34

---
+++ Ap34.js
@@ -0,0 +1,9 @@
+"use strict";
+// creating array of different names of pizza
+let pizza = ["malae", "supereme", "fajita"];
+//using for loop
+for (let onepizza of pizza) {
+    console.log(`I like ${onepizza} pizza`);
+}
+//print a message forloop outside
+console.log(`pizza is love`);
