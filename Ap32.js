65f594f30d610764c68dbb4256eef0762eab6494
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:46:21 -0700

    Assignment32

---
+++ Ap32.js
@@ -0,0 +1,15 @@
+"use strict";
+// array of current users
+let currentUsers = ["sukaina", "ashfa", "noveen", "kanwal", "asifa"];
+// array of new users
+let newUsers = ["usman", "ali", "arslan", "shahid", "maaz"];
+// loop for newUsers to check userNames availability
+newUsers.forEach(new_one_user => {
+    let our_condition = currentUsers.some(current_one_user => current_one_user.toLowerCase() === new_one_user.toLowerCase());
+    if (our_condition) {
+        console.log(`sorry ${new_one_user} is already taken!`);
+    }
+    else {
+        console.log(`This user ${new_one_user} is available.`);
+    }
+});
