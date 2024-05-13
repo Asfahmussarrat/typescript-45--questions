dc063cf925a702b39a1ace3e4dff149a73c150aa
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:11:05 -0700

    Add files via upload

---
+++ Ap21.js
@@ -0,0 +1,7 @@
+"use strict";
+let ITcourse = {
+    courseName: "typescript and javascript",
+    location: "Governor House",
+    onsitestudents: 50000
+};
+console.log(ITcourse);
---
+++ Ap21.ts
@@ -0,0 +1,13 @@
+interface ITcourse {
+    courseName: string;
+    location: string;
+    onsitestudents: number;
+
+}
+ let  ITcourse = {
+    courseName: "typescript and javascript",
+    location:"Governor House",
+    onsitestudents: 50000
+
+};
+console.log(ITcourse);
