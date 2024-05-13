9db9c1dc5369e4b24f03f591d15fec2be6c16d4e
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:18:41 -0700

    Add files via upload

---
+++ A23.ts
@@ -0,0 +1,41 @@
+//create a variable
+let five = 5;
+let ten = 10;
+//Testing var 1
+console.log("Test 1: five is equals to 5 ?");
+console.log(five == 5);
+//test 2
+console.log("test 2: ten is equal to 10 ?");
+
+console.log(ten == 10);
+//test 3
+console.log("test 3: 5 is not equal to 10 ?");
+
+
+console.log(five!= ten);
+//test 4
+console.log("test 4: 10 is greater than 5");
+console.log(ten > 5);
+//test 5
+console.log("test 5: 5 is smaller than 10");
+console.log(five < ten);
+//test 6
+console.log("test 6: 10 is smaller than 5");
+console.log(ten < 5);
+//test 7
+console.log("test 7: 5 is equal to 10");
+console.log(five == ten);
+
+
+//test 8
+
+console.log("test 8: 10 is not equal to 10");
+
+
+console.log(ten != ten);
+//test9
+console.log("test 9: 5 is greater than ten");
+console.log(five > ten);
+//test 10
+console.log("test 10: 100 is not equal to 50 ");
+console.log(100 < 50);
