d2b1d41e94eec8e991911392ab69dc0af0e164e2
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:32:46 -0700

    Assignment25

---
+++ A25.js
@@ -0,0 +1,10 @@
+"use strict";
+//define variable
+let alienColour = "black";
+if (alienColour === "black") {
+    console.log("Alien colour is black players just earned 5 points.");
+}
+//second part
+if (alienColour === "white") {
+    console.log("Alien colour is white");
+}
---
+++ A25.ts
@@ -0,0 +1,13 @@
+//define variable
+let alienColour = "black";
+
+
+if ( alienColour === "black"){
+    console.log("Alien colour is black players just earned 5 points.");
+
+}
+//second part
+if (alienColour === "white"){
+    console.log("Alien colour is white");
+
+}
