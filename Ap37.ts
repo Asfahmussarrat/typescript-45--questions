8a9d55693bbfc4c0b37d5702b8ac4413d352095f
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:52:51 -0700

    Assignment37

---
+++ Ap37.ts
@@ -0,0 +1,10 @@
+// making a FUNCTION
+function make_shirt(size:string ="large", printmessage:string = "I love typescript"){
+    console.log(`creating the size${size} shirt with the${printmessage}print on shirt`);
+}
+// calling the function
+make_shirt();
+//calling the function on medium size by default message
+make_shirt("medium")
+//calling a function by small size by new message
+make_shirt("small","I love A.I")
