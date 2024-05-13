a06b3c54866b3e1aa50d3d0fca7f07ef54959b2f
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:34:52 -0700

    Assignment26

---
+++ A26.ts
@@ -0,0 +1,58 @@
+let alienColour = "green";
+if (alienColour === "green"){
+    console.log("players need just 5 points for shooting the alien.");
+
+
+}
+ else {
+    console.log("players just earned 10 points.");
+}
+ //second version
+ if (alienColour === "blue");{
+    console.log("my colour is blue.");
+
+ }
+
+ else {
+    console.log("i m come from else statement");
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ //else {
+    console.log("i m come from else statement.");         
+ }
