ef87b69cf09d5ffda74cc069b82d93655389481f
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:13:16 -0700

    Add files via upload

---
+++ A22.js
@@ -0,0 +1,7 @@
+"use strict";
+//creaye an array
+let errorArray = ["a", "b", "c", "d", "e"];
+//print error in array
+console.log(errorArray[10]);
+//now print error without
+console.log(errorArray[1]);
---
+++ A22.ts
@@ -0,0 +1,6 @@
+//creaye an array
+let errorArray: string[] = ["a","b","c","d","e"];
+//print error in array
+console.log(errorArray[10]);
+//now print error without
+console.log(errorArray[1]);
