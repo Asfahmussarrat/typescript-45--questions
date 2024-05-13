28d4c6f85f5ca0ca636704c9cfe9839dc7bbf3bb
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 02:02:46 -0700

    Assignment44

---
+++ Ap44.ts
@@ -0,0 +1,10 @@
+//Making your sandwitches with
+function makeSandwitch(items:string[]){
+    console.log("make sandwitch with:");
+    items.forEach(element =>console.log("_"+element));
+    console.log("enjoy your sandwitch !\n");
+}
+makeSandwitch(["mayo","cheese","ketchup"]);
+makeSandwitch(["bread","butter"]);
+makeSandwitch(["egg","chicken","pepper"]);
+
