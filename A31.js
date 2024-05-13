19d6f84aa9c7b3c8f41af3dad8b8f27547e8cb33
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:42:17 -0700

    Assignment31

---
+++ A31.js
@@ -0,0 +1,17 @@
+"use strict";
+// creating an array
+let userName = ["sukaina", "ashfa", "noveen", "admin", "kanwal"];
+if (userName.length === 0) {
+    console.log("your array is empty we need to find user!");
+}
+else {
+    // using forEach loop on array
+    userName.forEach(oneuser => {
+        if (oneuser === "admin") {
+            console.log(`Hello ${oneuser},would you like to see a status report?`);
+        }
+        else {
+            console.log(`Hello ${oneuser},thank you for logging in again.`);
+        }
+    });
+}
