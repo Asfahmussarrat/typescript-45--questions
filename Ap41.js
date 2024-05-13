6420a34a5920f7e7db7301598187a14398c8de89
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 01:58:35 -0700

    Assignment41

---
+++ Ap41.js
@@ -0,0 +1,9 @@
+"use strict";
+// Making an Array of Magicians;
+let Magicians = ["Junaid Bangalli", "pooja Nagan", "haree Ratti", "sifferganda"];
+function show_Magicians(Magicians) {
+    Magicians.forEach(Element => {
+        console.log(Element);
+    });
+}
+show_Magicians(Magicians);
---
+++ Ap41.ts
@@ -0,0 +1,12 @@
+// Making an Array of Magicians;
+let Magicians : string [] = ["Junaid Bangalli","pooja Nagan","haree Ratti","sifferganda"];
+function show_Magicians(Magicians:string[]){
+    Magicians.forEach(Element =>{
+        console.log(Element);
+    });
+
+}
+show_Magicians(Magicians);
+
+ 
+
